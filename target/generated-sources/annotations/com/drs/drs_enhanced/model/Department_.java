6c05fdd9d5bc53d92dae09e3428427250a077925
Opny Tenishia <o.kamalenthiran@cqumail.com>
2025-06-10 12:18:52 +1000

    Adjusted Main Class in Pom

--- target/generated-sources/annotations/com/drs/drs_enhanced/model/Department_.java
+++ target/generated-sources/annotations/com/drs/drs_enhanced/model/Department_.java
@@ -6,7 +6,7 @@ import jakarta.persistence.metamodel.SingularAttribute;
 import jakarta.persistence.metamodel.StaticMetamodel;
 import javax.annotation.processing.Generated;
 
-@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-06-09T21:33:36", comments="EclipseLink-4.0.5.v20241223-ra96b873527f305f932543045c8679bb1de8d3a43")
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2025-06-10T12:03:08", comments="EclipseLink-4.0.5.v20241223-ra96b873527f305f932543045c8679bb1de8d3a43")
 @StaticMetamodel(Department.class)
 @SuppressWarnings({"rawtypes", "deprecation"})
 public class Department_ extends User_ {
